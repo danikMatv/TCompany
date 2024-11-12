d171cbd6fd9f69f52c10d1a72790629a86d1a1eb
daniilMatviichuk <168640672+daniilMatviichuk@users.noreply.github.com>
2024-11-12 13:05:15 +0200

    added tests

--- BTestProject2/GoodsServiceTests.cs
+++ BTestProject2/GoodsServiceTests.cs
@@ -10,7 +10,7 @@ namespace BTestProject2
     public class GoodsServiceTests
     {
         [Fact]
-        public void UpdateGoodsByGoodsId()
+        public void GetById_ReturnsCorrectGoods()
         {
             var mockGoodsDal = new Mock<IGoods>();
             var expectedGoods = new Goods { id = 3, name = "Item" };
@@ -23,6 +23,75 @@ namespace BTestProject2
             Assert.NotNull(goods);
             Assert.Equal(3, goods.id);
             Assert.Equal("Item", goods.name);
+        }
+
+        [Fact]
+        public void Create_CreatesNewGoods()
+        {
+            var mockGoodsDal = new Mock<IGoods>();
+            var goodsToCreate = new Goods { id = 4, name = "NewItem" };
+            mockGoodsDal.Setup(dal => dal.Create(It.IsAny<Goods>())).Returns(goodsToCreate);
+
+            var goodsService = new GoodsService(mockGoodsDal.Object);
+
+            var createdGoods = goodsService.Create(goodsToCreate);
+
+            Assert.NotNull(createdGoods);
+            Assert.Equal(4, createdGoods.id);
+            Assert.Equal("NewItem", createdGoods.name);
+        }
+
+        [Fact]
+        public void Update_UpdatesExistingGoods()
+        {
+            var mockGoodsDal = new Mock<IGoods>();
+            var updatedGoods = new Goods { id = 3, name = "UpdatedItem" };
+            mockGoodsDal.Setup(dal => dal.Update(3, It.IsAny<Goods>())).Returns(updatedGoods);
+
+            var goodsService = new GoodsService(mockGoodsDal.Object);
+
+            var result = goodsService.Update(3, updatedGoods);
+
+            Assert.NotNull(result);
+            Assert.Equal(3, result.id);
+            Assert.Equal("UpdatedItem", result.name);
+        }
+
+        [Fact]
+        public void Delete_DeletesGoods()
+        {
+            var mockGoodsDal = new Mock<IGoods>();
+            var goodsToDelete = new Goods { id = 3, name = "ItemToDelete" };
+            mockGoodsDal.Setup(dal => dal.Delete(3)).Returns(goodsToDelete);
+
+            var goodsService = new GoodsService(mockGoodsDal.Object);
+
+            var deletedGoods = goodsService.Delete(3);
+
+            Assert.NotNull(deletedGoods);
+            Assert.Equal(3, deletedGoods.id);
+            Assert.Equal("ItemToDelete", deletedGoods.name);
+        }
+
+        [Fact]
+        public void GetAll_ReturnsAllGoods()
+        {
+            var mockGoodsDal = new Mock<IGoods>();
+            var goodsList = new List<Goods>
+            {
+                new Goods { id = 1, name = "Item1" },
+                new Goods { id = 2, name = "Item2" }
+            };
+            mockGoodsDal.Setup(dal => dal.GetAll()).Returns(goodsList);
+
+            var goodsService = new GoodsService(mockGoodsDal.Object);
+
+            var result = goodsService.GetAll();
+
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count);
+            Assert.Contains(result, g => g.name == "Item1");
+            Assert.Contains(result, g => g.name == "Item2");
         }
     }
 }

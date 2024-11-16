57ff71ab24f5c6b2aa4a455c0e77ef3ea057e66c
daniilMatviichuk <168640672+daniilMatviichuk@users.noreply.github.com>
2024-11-17 00:14:07 +0200

    add redirect to start page

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,81 +1,81 @@
-using BLL.IServices;
-using BLL.Services;
-using DAL.Repository;
-using DALEF.Conc;
-using DALEF.Ct;
-using DALEF.Mapping;
-using DALEF.Models;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.EntityFrameworkCore;
+    using BLL.IServices;
+    using BLL.Services;
+    using DAL.Repository;
+    using DALEF.Conc;
+    using DALEF.Ct;
+    using DALEF.Mapping;
+    using DALEF.Models;
+    using Microsoft.AspNetCore.Authentication.Cookies;
+    using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
+    var builder = WebApplication.CreateBuilder(args);
 
-// Налаштування підключення до бази даних
-builder.Services.AddDbContext<ImdbContext>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseSqlServer(connectionString);  // Налаштування бази даних через DI
-});// Рекомендується реєструвати лише один контекст:
-builder.Services.AddDbContext<R2024Context>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    options.UseSqlServer(connectionString);  // Налаштування підключення до бази даних через DI
-});
-
-
-// Додавання служб для сесій
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromMinutes(30); // Час життя сесії
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-});
-
-// Налаштування аутентифікації з використанням Cookie
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-}).AddCookie(options =>
-{
-    options.LoginPath = "/Account/Login";
-    options.AccessDeniedPath = "/Account/AccessDenied";
-});
+    // Налаштування підключення до бази даних
+    builder.Services.AddDbContext<ImdbContext>(options =>
+    {
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        options.UseSqlServer(connectionString);  // Налаштування бази даних через DI
+    });// Рекомендується реєструвати лише один контекст:
+    builder.Services.AddDbContext<R2024Context>(options =>
+    {
+        var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        options.UseSqlServer(connectionString);  // Налаштування підключення до бази даних через DI
+    });
 
 
-// Додавання AutoMapper
-builder.Services.AddAutoMapper(typeof(Program));
-builder.Services.AddAutoMapper(typeof(MovieProfile));
-// Реєстрація залежностей
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IGoodsService, GoodsService>();
-builder.Services.AddScoped<IUsers, UsersDalEf>();
-builder.Services.AddScoped<IGoods, GoodsDalEf>();
+    // Додавання служб для сесій
+    builder.Services.AddSession(options =>
+    {
+        options.IdleTimeout = TimeSpan.FromMinutes(30); // Час життя сесії
+        options.Cookie.HttpOnly = true;
+        options.Cookie.IsEssential = true;
+    });
 
-// Додавання MVC
-builder.Services.AddControllersWithViews();
+    // Налаштування аутентифікації з використанням Cookie
+    builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    }).AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.AccessDeniedPath = "/Account/AccessDenied";
+    });
 
-var app = builder.Build();
 
-// Конфігурація Middleware
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
+    // Додавання AutoMapper
+    builder.Services.AddAutoMapper(typeof(Program));
+    builder.Services.AddAutoMapper(typeof(MovieProfile));
+    // Реєстрація залежностей
+    builder.Services.AddScoped<IUserService, UserService>();
+    builder.Services.AddScoped<IGoodsService, GoodsService>();
+    builder.Services.AddScoped<IUsers, UsersDalEf>();
+    builder.Services.AddScoped<IGoods, GoodsDalEf>();
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+    // Додавання MVC
+    builder.Services.AddControllersWithViews();
 
-app.UseRouting();
+    var app = builder.Build();
 
-// Використання аутентифікації та сесій
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseSession();
+    // Конфігурація Middleware
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+    }
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
 
-app.Run();
+    app.UseRouting();
+
+    // Використання аутентифікації та сесій
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.UseSession();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Account}/{action=Login}/{id?}");
+
+    app.Run();
